49426d1f2e7b45ff291ac380d2ab2317b7c6e717
Yn2o <130376508+Yn2o@users.noreply.github.com>
2025-02-26 09:26:30 +0100

    Create hello_github.py

---
+++ hello_github.py
@@ -0,0 +1,9 @@
+# programme Python pour GitHub
+
+def main():
+    print("Bienvenue sur mon premier projet GitHub !")
+    nom = input("Comment tu t'appelles ? ")
+    print(f"Enchanté, {Yn2o} ! J'espère que tu vas bien.")
+
+if __name__ == "__main__":
+    main()
